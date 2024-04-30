66fb1fdfde5c21621d143e92490121e9bd02c2a9
Rio Ferdinand <rioferdinandf53327@gmail.com>
2024-04-30 21:37:22 +0700

    Alamak Cafe

---
+++ resources/views/index.blade.php
@@ -0,0 +1,444 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Alamak Cafe</title>
+<meta name="description" content="">
+<meta name="author" content="">
+
+<!-- Favicons
+    ================================================== -->
+<link rel="shortcut icon" href="touche-master/img/favicon.ico" type="image/x-icon">
+<link rel="apple-touch-icon" href="touche-master/img/apple-touch-icon.png">
+<link rel="apple-touch-icon" sizes="72x72" href="touche-master/img/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="114x114" href="touche-master/img/apple-touch-icon-114x114.png">
+
+<!-- Bootstrap -->
+<link rel="stylesheet" type="text/css"  href="touche-master/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="touche-master/fonts/font-awesome/css/font-awesome.css">
+
+<!-- Stylesheet
+    ================================================== -->
+<link rel="stylesheet" type="text/css"  href="touche-master/css/style.css">
+<link rel="stylesheet" type="text/css" href="touche-master/css/nivo-lightbox/nivo-lightbox.css">
+<link rel="stylesheet" type="text/css" href="touche-master/css/nivo-lightbox/default.css">
+<link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,600,700" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Dancing+Script:400,700" rel="stylesheet">
+</head>
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+<!-- Navigation
+    ==========================================-->
+<nav id="menu" class="navbar navbar-default navbar-fixed-top">
+  <div class="container"> 
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+    
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#about" class="page-scroll">About</a></li>
+        <li><a href="#restaurant-menu" class="page-scroll">Menu</a></li>
+        <li><a href="#portfolio" class="page-scroll">Gallery</a></li>
+        <li><a href="#team" class="page-scroll">Chefs</a></li>
+        <li><a href="#call-reservation" class="page-scroll">Contact</a></li>
+      </ul>
+    </div>
+    <!-- /.navbar-collapse --> 
+  </div>
+</nav>
+<!-- Header -->
+<header id="header">
+  <div class="intro">
+    <div class="overlay">
+      <div class="container">
+        <div class="row">
+          <div class="intro-text">
+            <!-- <h1>Alamak!</h1> -->
+            <a href="#"><img src="assets/logo.png" alt="logo"></a>
+            <p>Restaurant & Coffee</p>
+            <a href="#about" class="btn btn-custom btn-lg page-scroll">Pesen Gk Cuy?</a> </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
+<!-- About Section -->
+<div id="about">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-md-6 ">
+        <div class="about-img"><img src="assets/jongun.jpg" class="img-responsive" alt=""></div>
+      </div>
+      <div class="col-xs-12 col-md-6">
+        <div class="about-text">
+          <h2>Our Restaurant</h2>
+          <hr>
+          <p>Our café is the best café in Bandung, our special menu is Robuca Arabasta Coffee served by our beautiful and charming waiters.</p>
+          <h3>Awarded Chefs</h3>
+          <p>Approved by Kim Jong-Un</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Restaurant Menu Section -->
+<div id="restaurant-menu">
+  <div class="section-title text-center center">
+    <div class="overlay">
+      <h2>Menu</h2>
+      <hr>
+      <p>Free to saw our menu. All is Special! hehe</p>
+    </div>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <div class="menu-section">
+          <h2 class="menu-section-title">Breakfast & Starters</h2>
+          <hr>
+          <div class="menu-item">
+            <div class="menu-item-name"> Chocolate Chips Banana Bread </div>
+            <div class="menu-item-price"> 27K </div>
+            <div class="menu-item-description"> A bread that combines a Chocolate Chips and banana. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name"> Omelet </div>
+            <div class="menu-item-price"> 20K</div>
+            <div class="menu-item-description"> Basically Telor Dadar. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name"> Cheesy egg toast </div>
+            <div class="menu-item-price"> 17K </div>
+            <div class="menu-item-description"> A bread that covered with egg and melted cheese. </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-xs-12 col-sm-6">
+        <div class="menu-section">
+          <h2 class="menu-section-title">Main Course</h2>
+          <hr>
+          <div class="menu-item">
+            <div class="menu-item-name"> Borscht </div>
+            <div class="menu-item-price"> 30K </div>
+            <div class="menu-item-description"> A big sausage in sauce soup. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name"> Tom Yam </div>
+            <div class="menu-item-price"> 25K </div>
+            <div class="menu-item-description"> A soup that includes many things eatable. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name"> Shawarma </div>
+            <div class="menu-item-price"> 19K </div>
+            <div class="menu-item-description"> Mouth-watering dish of sliced, roasted meat. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name"> Ramen </div>
+            <div class="menu-item-price"> 35K </div>
+            <div class="menu-item-description">  noodle soup from japan. </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <div class="menu-section">
+          <h2 class="menu-section-title">Dinner</h2>
+          <hr>
+          <div class="menu-item">
+            <div class="menu-item-name"> Croissant </div>
+            <div class="menu-item-price"> 21K </div>
+            <div class="menu-item-description"> A French pastry made from puff pastry in a crescent shape. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name"> Dim sum </div>
+            <div class="menu-item-price"> 15K</div>
+            <div class="menu-item-description"> Traditional Chinese meal of small plates of savory and sweet dishes. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name"> Canai Bread </div>
+            <div class="menu-item-price"> 20K </div>
+            <div class="menu-item-description"> an Indian flatbread dish from Brunei. </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-xs-12 col-sm-6">
+        <div class="menu-section">
+          <h2 class="menu-section-title">Coffee & Drinks</h2>
+          <hr>
+          <div class="menu-item">
+            <div class="menu-item-name"> Moccachino </div>
+            <div class="menu-item-price"> 15K </div>
+            <div class="menu-item-description">  A variant of a caffè latte. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name"> Espresso </div>
+            <div class="menu-item-price"> 21K</div>
+            <div class="menu-item-description">  A coffee-brewing method in which a small amount of nearly boiling water is forced under pressure through finely ground coffee beans. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name"> Lemon Tea </div>
+            <div class="menu-item-price"> 10K </div>
+            <div class="menu-item-description"> Lemon flavoured tea. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name"> Liong Coffee </div>
+            <div class="menu-item-price"> 30K </div>
+            <div class="menu-item-description"> A traditional coffee on Bogor. </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Portfolio Section -->
+<div id="portfolio">
+  <div class="section-title text-center center">
+    <div class="overlay">
+      <h2>Gallery</h2>
+      <hr>
+      <p>You can see our menu here too.</p>
+    </div>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="categories">
+        <ul class="cat">
+          <li>
+            <ol class="type">
+              <li><a href="#" data-filter="*" class="active">All</a></li>
+              <li><a href="#" data-filter=".breakfast">Breakfast</a></li>
+              <li><a href="#" data-filter=".lunch">Lunch</a></li>
+              <li><a href="#" data-filter=".dinner">Dinner</a></li>
+            </ol>
+          </li>
+        </ul>
+        <div class="clearfix"></div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="portfolio-items">
+        <div class="col-sm-6 col-md-4 col-lg-4 breakfast">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/01-large.jpg" title="Dish Name" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4>Chocolate Chips Banana Bread</h4>
+              </div>
+              <img src="assets/chocobanana.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-4 dinner">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/01-large.jpg" title="Dish Name" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4>Croissant</h4>
+              </div>
+              <img src="assets/croissant.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-4 breakfast">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/03-large.jpg" title="Dish Name" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4>Omellete</h4>
+              </div>
+              <img src="assets/omellete.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-4 breakfast">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/04-large.jpg" title="Dish Name" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4>Cheesy Egg Bread</h4>
+              </div>
+              <img src="assets/cheesy.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-4 dinner">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/05-large.jpg" title="Dish Name" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4>Dim Sum</h4>
+              </div>
+              <img src="assets/dimsum.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-4 lunch">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/06-large.jpg" title="Dish Name" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4>Borscht</h4>
+              </div>
+              <img src="assets/borstch.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-4 lunch">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/07-large.jpg" title="Dish Name" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4>Tom Yam</h4>
+              </div>
+              <img src="assets/tomyam.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-4 dinner">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/09-large.jpg" title="Dish Name" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4>Canai bread</h4>
+              </div>
+              <img src="assets/canai.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-4 lunch">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/10-large.jpg" title="Dish Name" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4>Shawarma</h4>
+              </div>
+              <img src="assets/shawarma.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-4 lunch">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/12-large.jpg" title="Dish Name" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4>Ramen</h4>
+              </div>
+              <img src="assets/ramen.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Team Section -->
+<div id="team" class="text-center">
+  <div class="overlay">
+    <div class="container">
+      <div class="col-md-10 col-md-offset-1 section-title">
+        <h2>Meet Our Chefs</h2>
+        <hr>
+        <p>Meet our trusted internal chefs.</p>
+      </div>
+      <div id="row">
+        <div class="col-md-4 team">
+          <div class="thumbnail">
+            <div class="team-img"><img src="assets/Dubz.jpeg" alt="..."></div>
+            <div class="caption">
+              <h3>DubZ</h3>
+              <p>An Elite Chef who specialist at Dessert.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 team">
+          <div class="thumbnail">
+            <div class="team-img"><img src="assets/Stumelu.jpeg" alt="..."></div>
+            <div class="caption">
+              <h3>Stumelu</h3>
+              <p>An elite chef who specialist at Main Menu.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 team">
+          <div class="thumbnail">
+            <div class="team-img"><img src="assets/Messes.jpeg" alt="..."></div>
+            <div class="caption">
+              <h3>Messes</h3>
+              <p>An elite chef who specialist at Appetizer.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Call Reservation Section -->
+<div id="call-reservation" class="text-center">
+  <div class="container">
+    <h2>Want to Reserve?<strong> Go Call Us Dude!</strong></h2>
+  </div>
+</div>
+<!-- Contact Section -->
+<div id="contact" class="text-center">
+  <div class="container">
+    <div class="section-title text-center">
+      <h2>Contact Form</h2>
+      <hr>
+      <p>If you have an advice or critic,Feel free to send it to us :)</p>
+    </div>
+    <div class="col-md-10 col-md-offset-1">
+      <form name="sentMessage" id="contactForm" novalidate>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <input type="text" id="name" class="form-control" placeholder="Name" required="required">
+              <p class="help-block text-danger"></p>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="form-group">
+              <input type="email" id="email" class="form-control" placeholder="Email" required="required">
+              <p class="help-block text-danger"></p>
+            </div>
+          </div>
+        </div>
+        <div class="form-group">
+          <textarea name="message" id="message" class="form-control" rows="4" placeholder="Message" required></textarea>
+          <p class="help-block text-danger"></p>
+        </div>
+        <div id="success"></div>
+        <button type="submit" class="btn btn-custom btn-lg">Send Message</button>
+      </form>
+    </div>
+  </div>
+</div>
+<div id="footer">
+  <div class="container text-center">
+    <div class="col-md-4">
+      <h3>Address</h3>
+      <div class="contact-item">
+        <p>Cihampelas,</p>
+        <p>Bandung, Konohanesia</p>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <h3>Opening Hours</h3>
+      <div class="contact-item">
+        <p>Mon-Thurs: 10:00 AM - 11:00 PM</p>
+        <p>Fri-Sun: 11:00 AM - 02:00 AM</p>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <h3>Contact Info</h3>
+      <div class="contact-item">
+        <p>Phone: +62 8996 8100 44</p>
+        <p>Email: VerCorp@company.com</p>
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid text-center copyrights">
+    <div class="col-md-8 col-md-offset-2">
+      <div class="social">
+        <ul>
+          <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+          <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+          <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+        </ul>
+      </div>
+      <!-- <p>&copy; 2016 Touché. All rights reserved. Designed by <a href="http://www.templatewire.com" rel="nofollow">TemplateWire</a></p> -->
+    </div>
+  </div>
+</div>
+<script type="text/javascript" src="touche-master/js/jquery.1.11.1.js"></script> 
+<script type="text/javascript" src="touche-master/js/bootstrap.js"></script> 
+<script type="text/javascript" src="touche-master/js/SmoothScroll.js"></script> 
+<script type="text/javascript" src="touche-master/js/nivo-lightbox.js"></script> 
+<script type="text/javascript" src="touche-master/js/jquery.isotope.js"></script> 
+<script type="text/javascript" src="touche-master/js/jqBootstrapValidation.js"></script> 
+<script type="text/javascript" src="touche-master/js/contact_me.js"></script> 
+<script type="text/javascript" src="touche-master/js/main.js"></script>
+</body>
+</html>
